7ef99d09f73f9fea8ab919c281486b87911b8913
Bhargava Shastry <bshastry@ethereum.org>
2019-02-14 16:17:16 +0100

    Fuzz+Interpret WIP

--- test/tools/ossfuzz/strictasm_opt_ossfuzz.cpp
+++ test/tools/ossfuzz/strictasm_opt_ossfuzz.cpp
@@ -15,10 +15,66 @@
 	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <test/tools/yulInterpreter/Interpreter.h>
+
+#include <libyul/AsmAnalysisInfo.h>
+#include <libyul/AsmParser.h>
+#include <libyul/AsmAnalysis.h>
+#include <libyul/Dialect.h>
+#include <libyul/backends/evm/EVMDialect.h>
 #include <libyul/AssemblyStack.h>
+
+#include <liblangutil/Exceptions.h>
+#include <liblangutil/ErrorReporter.h>
+#include <liblangutil/EVMVersion.h>
+#include <liblangutil/SourceReferenceFormatter.h>
+
+#include <libdevcore/CommonIO.h>
+#include <libdevcore/CommonData.h>
+
+#include <string>
+#include <memory>
+#include <iostream>
 
 using namespace yul;
 using namespace std;
+
+using namespace langutil;
+using namespace dev;
+using namespace yul::test;
+
+void printErrors(ErrorList const& _errors)
+{
+	for (auto const& error: _errors)
+		SourceReferenceFormatter(cout).printExceptionInformation(
+				*error,
+				(error->type() == Error::Type::Warning) ? "Warning" : "Error"
+		);
+}
+
+void interpret(ostream& os, shared_ptr<Block> ast)
+{
+	InterpreterState state;
+	state.maxTraceSize = 10000;
+	Interpreter interpreter(state);
+	try
+	{
+		interpreter(*ast);
+	}
+	catch (InterpreterTerminated const&)
+	{
+	}
+
+	os << "Trace:" << endl;
+	for (auto const& line: interpreter.trace())
+		os << "  " << line << endl;
+	cout << "Memory dump:" << endl;
+	for (size_t i = 0; i < state.memory.size(); i += 0x20)
+		os << "  " << std::hex << std::setw(4) << i << ": " << toHex(bytesConstRef(state.memory.data() + i, 0x20).toBytes()) << endl;
+	os << "Storage dump:" << endl;
+	for (auto const& slot: state.storage)
+		os << "  " << slot.first.hex() << ": " << slot.second.hex() << endl;
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* _data, size_t _size)
 {
@@ -26,11 +82,22 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* _data, size_t _size)
 		return 0;
 
 	string input(reinterpret_cast<char const*>(_data), _size);
-	AssemblyStack stack(dev::solidity::EVMVersion::constantinople(), AssemblyStack::Language::StrictAssembly);
 
-	if (!stack.parseAndAnalyze("source", input))
+	AssemblyStack stack(solidity::EVMVersion::constantinople(), AssemblyStack::Language::StrictAssembly);
+	if (stack.parseAndAnalyze("source", input))
+		yulAssert(stack.errors().empty(), "Parsed successfully but had errors.");
+	else
+		printErrors(stack.errors());
+
+	if (!stack.parserResult()->code || !stack.parserResult()->analysisInfo)
 		return 0;
 
+	ostream os1, os2;
+	interpret(os1, stack.parserResult()->code);
 	stack.optimize();
+	interpret(os2, stack.parserResult()->code);
+
+	bool isTraceEq = (os2.str().compare(os1.str()) == 0);
+	yulAssert(isTraceEq, "Interpreted traces for optimized and unoptimized code differ.");
 	return 0;
 }
