6891f123164f41b3986ec3f29b5c540587882c87
Bhargava Shastry <bshastry@ethereum.org>
2019-02-14 15:42:24 +0100

    Patched strict asm fuzzing code

---
+++ test/tools/ossfuzz/strictasm_assembly_ossfuzz.cpp
@@ -0,0 +1,46 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <libyul/AssemblyStack.h>
+#include <libyul/backends/evm/EVMCodeTransform.h>
+
+using namespace yul;
+using namespace std;
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* _data, size_t _size)
+{
+	if (_size > 450)
+		return 0;
+
+	string input(reinterpret_cast<char const*>(_data), _size);
+	AssemblyStack stack(dev::solidity::EVMVersion::constantinople(), AssemblyStack::Language::StrictAssembly);
+
+	if (!stack.parseAndAnalyze("source", input))
+		return 0;
+
+	try
+	{
+		MachineAssemblyObject obj = stack.assemble(AssemblyStack::Machine::EVM);
+		solAssert(obj.bytecode, "");
+	}
+	catch (StackTooDeepError const&)
+	{
+
+	}
+
+	return 0;
+}
